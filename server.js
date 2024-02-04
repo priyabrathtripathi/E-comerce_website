0d7fdcccb895a149e8a51c0e0f6bafdc8b0dc84a
Priyabrath Tripathi <78248484+priyabrathtripathi@users.noreply.github.com>
2024-02-04 15:57:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ import categoryRoutes from './routes/categoryRoute.js'
 import productRoutes from './routes/productRoute.js'
 import cors from "cors"
 import path from 'path'
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
 
 //database
 connectDB();
+//esmodule fix error
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
 //middleware
